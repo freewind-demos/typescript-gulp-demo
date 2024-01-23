c6c8c4ec355895e711b5b5ee89ee6ea5a4158680
Peng Li <nowindlee@gmail.com>
2024-01-23 10:54:16 +0800

    upgrade dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp')
 const minify = require('gulp-minify')
 
-gulp.task('default', function () {
-    gulp.src('hello.js')
+gulp.task('default', () => {
+    return gulp.src('hello.js')
         .pipe(minify())
         .pipe(gulp.dest('build/'))
 })
